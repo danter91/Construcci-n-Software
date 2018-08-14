58b6f967408cdf9e14aa7164178175d1d63dc0e3
Daniel Baquero Ruiz <38636791+danter91@users.noreply.github.com>
2018-08-13 22:18:48 -0500

    Ejercicios Individuales

--- EjerciciosGuia2/Ejercicio01/src/Guia02/Ejercicio03.java
+++ EjerciciosGuia2/Ejercicio01/src/Guia02/Ejercicio03.java
@@ -3,12 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package Guia02;
+
+import java.util.Scanner;
 
 /**
  *
- * @author Administrator
+ * @author Daniel Baquero Ruiz, Universidad EAN 2018
  */
 public class Ejercicio03 {
-    
+ public static void main(String[] args) {
+  Scanner entrada = new Scanner (System.in); 
+  double capacidad;
+  double turistas;
+  double viajes;
+  double valortotalpasajes;
+  double valorcomision;
+  System.out.println("Programa que calcula la productividad de un colectivo según número de viajes y de pasajeros.");
+  System.out.println("Escriba la capacidad máxima del bus (número de personas)");
+  capacidad = entrada.nextDouble();
+  System.out.println("Escriba el número de turistas que van al hotel:");
+  turistas = entrada.nextDouble();
+  viajes = Math.ceil(turistas/capacidad);
+  valortotalpasajes = turistas*10000;
+  valorcomision = turistas*2000;
+  System.out.println("El bus debe realizar "+viajes+" viajes, el valor total que deben pagar todos los turistas al conductor es "+valortotalpasajes+" y la comisión que debe pagar el conductor a la empresa propietaria es "+valorcomision+".");
+  
+}
 }
--- EjerciciosGuia2/Ejercicio01/src/Guia02/Ejercicio04.java
+++ EjerciciosGuia2/Ejercicio01/src/Guia02/Ejercicio04.java
@@ -5,10 +5,55 @@
  */
 package Guia02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Administrator
  */
 public class Ejercicio04 {
-    
+    public static void main(String[] args) {
+Scanner entrada = new Scanner (System.in); 
+  double preciobruto;
+  String tipovehiculo;
+  double impuesto;
+  double precioneto;
+  System.out.println("Programa que calcula el impuesto a pagar y el precio neto de un vehiculo según categoría.");
+  System.out.println("Escriba el precio bruto del vehículo:");
+  preciobruto = entrada.nextDouble();
+  System.out.println("Escriba el tipo de vehículo: (‘C’ para camioneta o ‘A’ para automóvil,");
+  tipovehiculo = entrada.next();
+  
+  
+  if(tipovehiculo.equalsIgnoreCase("C")){
+  if(preciobruto<80){
+  System.out.println("El impuesto a pagar por la camioneta es $0 y el precio neto es $"+preciobruto+".");
+  }
+  else{
+  impuesto = preciobruto/2;
+  precioneto = impuesto+preciobruto;
+  System.out.println("El impuesto a pagar por la camioneta es $"+impuesto+" y el precio neto es $"+precioneto+".");
+  }
+  }
+  else if (tipovehiculo.equalsIgnoreCase("A")){
+  if(preciobruto<=20){
+  impuesto = 5;
+  precioneto = impuesto+preciobruto;
+  System.out.println("El impuesto a pagar por el automóvil es $"+impuesto+" y el precio neto es $"+precioneto+".");
+  }
+  else if(preciobruto>20&&preciobruto<=40){
+  impuesto = preciobruto/5;
+  precioneto = impuesto+preciobruto;
+  System.out.println("El impuesto a pagar por el automóvil es $"+impuesto+" y el precio neto es $"+precioneto+".");
+  }
+  else if(preciobruto>40){
+  impuesto = 9;
+  precioneto = impuesto+preciobruto;
+  System.out.println("El impuesto a pagar por el automóvil es $"+impuesto+" y el precio neto es $"+precioneto+".");
+  } 
+  }
+  else {
+  System.out.println("El valor ingresado no corresponde a un tipo de vehículo válido.");    
+  }
+}
 }
